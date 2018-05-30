5fc05581b80f41c36ae78451fcaed2ba5dd34ae0
Th1o <31651108+Th1o@users.noreply.github.com>
2018-05-30 16:10:19 +0400

    Filter.js refactored

--- lab-2/src/filter.js
+++ lab-2/src/filter.js
@@ -1,40 +1,26 @@
-export let filter = function f(list,val="noname") {
-    let sortedList = [];
-    let lowVal = val.toLowerCase();
-    let index = lowVal.indexOf(':');
-    let subVal = lowVal.substr(index + 1);
+export let filter = (list, val = "noname") => {
+    let sortedList = [],
+        lowVal = val.toLowerCase();
 
 
-if (val.includes(':')) {
+    if (val.includes(':')) {
+        const splitted = lowVal.split(':');
+        list.forEach(el => {
+            let lowEl = el[splitted[0]].toLowerCase();
+            if (lowEl.includes(splitted[1])) {
+                sortedList.push(el);
+            }
+        })
+        return sortedList;
+    }
+    ;
+
     list.forEach(el => {
-        if (val.startsWith('name:')) {
-            let lowName = el.name.toLowerCase();
-            if (lowName.includes(subVal)) {
-                sortedList.push(el);    
-            }
-        }
-        if (val.startsWith('email:')) {
-            let lowEmail = el.email.toLowerCase();
-            if (lowEmail.includes(subVal)) {
-                sortedList.push(el);
-            }
-
-        }
-        if (val.startsWith('phone:')) {
-            if (el.phone.includes(subVal)) {
-                sortedList.push(el);
-            }
-        }  
-    })
-    return sortedList;
-};
-
-    list.forEach(el=> {
         if (el.name.toLowerCase().includes(lowVal)) {
             sortedList.push(el);
         }
     })
     return sortedList;
 
-    
+
 };
